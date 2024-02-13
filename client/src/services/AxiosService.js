04312efc1e3765340eb394a9c220690b12c11cfb
Corinna Bolon <kyndrill@rocketmail.com>
2024-02-12 21:01:28 -0800

    New API

--- client/src/services/AxiosService.js
+++ client/src/services/AxiosService.js
@@ -16,7 +16,7 @@ export const sandboxApi = Axios.create({
 export const weatherApi = Axios.create({
   baseURL: 'https://api.tomorrow.io/v4/weather',
   timeout: 6000,
-  param: { apikey: "pasteinhere" }
+  param: { apikey: "pasteinhe" }
 })
 
 api.interceptors.request.use(config => config, handleAxiosError)
