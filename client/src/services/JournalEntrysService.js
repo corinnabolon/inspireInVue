f720e8e781a1dc8c206e2032001446058b5546ec
Corinna Bolon <kyndrill@rocketmail.com>
2024-04-12 22:29:10 -0700

    Got same date entries not to post

--- client/src/services/JournalEntrysService.js
+++ client/src/services/JournalEntrysService.js
@@ -12,10 +12,27 @@ class JournalEntrysService {
   }
 
   async submitJournal(journalData) {
-    const res = await api.post(`api/journalentrys`, journalData)
-    logger.log("Created journal entry", res.data)
-    AppState.journalEntrys.push(new JournalEntry(res.data))
-    logger.log("Journal entrys", AppState.journalEntrys)
+    let cancel = false
+    let newDate = new Date().toUTCString()
+    let newDateString = newDate.substring(0, 16)
+    let dayOfWeek = newDateString.substring(0, 3)
+    let dateNum = newDateString.substring(5, 7)
+    let month = newDateString.substring(8, 11)
+    let year = newDateString.substring(12, 16)
+    let rearrangedDateString = dayOfWeek + " " + month + " " + dateNum + " " + year
+    AppState.journalEntrys.forEach((journalEntry) => {
+      if (journalEntry.createdAt == rearrangedDateString) {
+        logger.log("Same date")
+        cancel = true
+        return
+      }
+    })
+    if (!cancel) {
+      const res = await api.post(`api/journalentrys`, journalData)
+      logger.log("Created journal entry", res.data)
+      AppState.journalEntrys.push(new JournalEntry(res.data))
+      logger.log("Journal entrys", AppState.journalEntrys)
+    }
   }
 
 }
